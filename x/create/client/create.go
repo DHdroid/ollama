f8bfb9be7646b132388675257866aa58f6d581b1
DHdroid <happydh1@snu.ac.kr>
2026-05-15 16:55:43 +0900

    Add Parsers

--- x/create/client/create.go
+++ x/create/client/create.go
@@ -662,6 +662,7 @@ func supportsThinking(modelDir string) bool {
 	thinkingArchitectures := []string{
 		"glm4moe",  // GLM-4 MoE models
 		"deepseek", // DeepSeek models
+		"exaone4",  // EXAONE 4.x models
 		"qwen3",    // Qwen3 models
 	}
 
